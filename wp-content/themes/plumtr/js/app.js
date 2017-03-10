f2f78b37f3730f58afed535891abc786ddbf5b3d
_null <i-do-wordpress@wp.pl>
2017-03-10 18:55:07 +0000

    well even height fixed

--- wp-content/themes/plumtr/js/app.js
+++ wp-content/themes/plumtr/js/app.js
@@ -38,21 +38,23 @@
     
   p.controller('CtrlRoot', CtrlRoot);
   
-  p.$inject = ['$http', '$scope'];
+  p.$inject = ['$http', '$scope', '$rootScope'];
   
-  function CtrlRoot($http, $scope){
+  function CtrlRoot($http, $scope, $rootScope){
     var self = this;
     self.posts = [];
     
     $http.get(baseRest+'/posts?per_page=20').then(function(response){
       if(response.data.length){
         self.posts = response.data;
-        console.log(self.posts[0]);
-        console.log(self.posts.length);
+        //console.log(self.posts[0]);
+        //console.log(self.posts.length);
       }
     });
     
-  
+    
+    
+    //set even height for all items
     $scope.$watch(
       function(){
         var wells = angular.element(document).find('._well');
@@ -64,14 +66,22 @@
         var wells = angular.element(document).find('._well');
         var makeEven = 0;
         //wells.matchHeight();
-        for(var i = 0; i<len; i++){
-          makeEven = makeEven < wells[i].offsetHeight ? wells[i].offsetHeight : makeEven;
-        }
-        for(var i = 0; i<len; i++){
-         $(wells[i]).outerHeight(makeEven);
-        }
+        
+        angular.element(document).find('img.thumb100').load(
+          function(){
+            for(var i = 0; i<len; i++){
+              makeEven = makeEven < wells[i].offsetHeight ? wells[i].offsetHeight : makeEven;
+            }
+            //console.log(makeEven);
+            for(var i = 0; i<len; i++){
+              $(wells[i]).outerHeight(makeEven);
+              //$(wells[i]).attr('min-height', '120px');
+              //$rootScope.$apply();
+            }
+          }  
+        );
       }
-    );
+    ); //end watch
     
     
     
